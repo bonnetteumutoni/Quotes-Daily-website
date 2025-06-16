7e7427ca7358796cb363d3df7ede5d99f51a8fe2
Bonnette <bonnetteumu@gmail.com>
2025-06-16 17:33:12 +0300

    added new changes

--- quote.js
+++ quote.js
@@ -1,11 +1,10 @@
 
-
 const quotesContainer=document.getElementById("quotes-container");
 
 const getQuotes=async ()=>{
 
     try{
-        const response=await fetch("https://dummyjson.com/quotes?limit=10000",{
+        const response=await fetch("https://dummyjson.com/quotes?limit=20",{
             method:"GET",
             headers:{
                 "Content-type":"application/json"
@@ -27,21 +26,27 @@ const dailyQuotes=async () =>{
     const words=document.createElement("p");
     const author=document.createElement("p");
     const container=document.createElement("div");
+    const icon = document.createElement("button")
 
     container.appendChild(words);
     container.appendChild(author);
+    container.appendChild(icon)
 
     words.textContent=`${item.quote}`;
     author.textContent=`Author:${item.author}`;
+    icon.textContent = "Add To Favorites"
 
     words.style.fontWeight="Bold";
-    words.style.color="Maroon";
+    words.style.color="rgb(43, 7, 24)";
     words.style.paddingTop="40px";
     words.style.paddingLeft="40px";
     words.style.fontSize="22px"
     
     author.style.paddingLeft="40px";
     author.style.paddingBottom="40px";
+    icon.style.width = "20%"
+    icon.style.color = "white"
+    icon.style.marginBottom = "20px"
 
     container.setAttribute("class","quotes-carp")
     quotesContainer.appendChild(container);
@@ -50,5 +55,47 @@ const dailyQuotes=async () =>{
 }
 
 dailyQuotes();
+const searchInput = document.getElementById('search');
 
+searchInput.addEventListener('keyup', () => {
+  const filter = searchInput.value.toLowerCase();
+  const items = quotesDaily.querySelectorAll('p');
 
+  items.forEach(item => {
+    const text = item.textContent.toLowerCase();
+    if (text.includes(filter)) {
+      item.style.display = '';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+});
+
+// let currentIndex = 0;
+// const batchSize = 8;
+
+// const quoteList = document.getElementById('quotes-container');
+// const readMoreBtn = document.getElementById('read-more');
+
+// function displayQuotes() {
+//   const nextBatch = quotesDaily.slice(currentIndex, currentIndex + batchSize);
+
+//   nextBatch.forEach(quote => {
+//     const li = document.createElement('li');
+//     li.className = 'quote-item';
+//     li.innerHTML = `
+//       <p>${item.quote}</p>
+//       <p><em>${item.author ? item.author : 'Unknown'}</em></p>
+//     `;
+//     quoteList.appendChild(li);
+//   });
+
+//   currentIndex += batchSize;
+
+//   if (currentIndex >= quotesDaily.length) {
+//     readMoreBtn.style.display = 'none';
+//   }
+// }
+// readMoreBtn.addEventListener('click', () => {
+//   displayQuotes();
+// });
